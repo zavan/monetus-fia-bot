19417e20af3b700b031f2dfe6ccac510aaeec8d5
Jhonny Moreira <me.jhonnymoreira@gmail.com>
2018-06-04 00:27:47 -0300

    feat(utils): `generateAsciiTableImageBuffer`

---
+++ tests/utils/generateAsciiTableImageBuffer.test.js
@@ -0,0 +1,14 @@
+const generateAsciiTableImageBuffer = require('../../lib/utils/generateAsciiTableImageBuffer')
+
+test('utils.generateAsciiTableImageBuffer', () => {
+  const text = `┌────────┬───────────────┬────────┬────────┐
+│ Ticker │ Asset         │      % │      Δ │
+├────────┼───────────────┼────────┼────────┤
+│ LEET1  │ 1337! Corp.   │ 50.10% │ +0.13% │
+│ OCTO3  │ Octocat Corp. │ 49.90% │ -0.05% │
+├────────┼───────────────┼────────┼────────┤
+│ TOTAL  │               │   100% │ +0.08% │
+└────────┴───────────────┴────────┴────────┘`
+
+  expect(generateAsciiTableImageBuffer(text)).toMatchSnapshot()
+})
